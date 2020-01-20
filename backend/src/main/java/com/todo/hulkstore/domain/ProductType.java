e0667b1c0ebbd2258ec636f66b527fd288461e6c
Cesarlp <cesarlp.ng@gmail.com>
2020-01-20 07:56:57 -0300

    Backend: fixed one to many in product type

--- backend/src/main/java/com/todo/hulkstore/domain/ProductType.java
+++ backend/src/main/java/com/todo/hulkstore/domain/ProductType.java
@@ -6,11 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 import static com.todo.hulkstore.utils.ValidationUtils.checkNotNullNorEmpty;
 
@@ -32,6 +36,14 @@ public class ProductType {
 
     @Setter(value = AccessLevel.NONE)
     String name;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            mappedBy = "productType",
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private Set<Product> product;
 
     public void setName(String name) {
         checkNotNullNorEmpty(name, "Name cannot be null nor empty.");
